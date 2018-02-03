347701003945989670120497fb435c1abe3f461d
Robert W <robwash@gmail.com>
2018-02-03 01:57:08 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,12 @@
 require "pry"
 
 def square_array(array)
-    #square_num = []
-      array.collect { |square| square **2  }
-    #      square_num << square ** 2
 
-        #end
-    #square_num
+    square_num = []
+      array.each do |square|
+          square_num << square ** 2
+          #binding.pry
+        end
+    square_num
+
 end
