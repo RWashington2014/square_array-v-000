712e5289b0850acf30e4fed1380f3c43042713f4
Robert W <robwash@gmail.com>
2018-02-03 01:56:27 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -4,7 +4,7 @@ def square_array(array)
     #square_num = []
       array.collect { |square| square **2  }
     #      square_num << square ** 2
-          #binding.pry
-        end
-  #square_num
+
+        #end
+    #square_num
 end
